85156bfc24cde2cf77945a1fe4195602c2bc652e
CristianN998 <cristiancasallas998@gmail.com>
2023-09-11 16:28:58 -0500

    add movie modal form

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,192 @@
 <script setup lang="ts">
 import { items } from "./movies.json";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 /*
  This is an Icon that you can use to represent the stars if you like
  otherwise you could just use a simple ⭐️ emoji, or * character.
 */
 import { StarIcon } from "@heroicons/vue/24/solid";
-
 const movies = reactive(items);
 
-const handleClick = (event: Event, number) => {
-  (event.target as HTMLButtonElement).disabled = true;
-  console.log(number);
+function updateRating(movieIndex, rating) {
+  movies[movieIndex].rating = rating;
+}
 
+
+const errors = reactive({
+  name: null,
+  description: null,
+  image: null,
+  inTheaters: null,
+  genres: null,
+});
+const form = reactive({
+  name: null,
+  description: null,
+  image: null,
+  inTheaters: false,
+  genres: [],
+});
+const validations = reactive({
+  name: "required",
+  genres: "required",
+});
+
+const genres = reactive([
+  { text: "Drama", value: "Drama" },
+  { text: "Crime", value: "Crime" },
+  { text: "Action", value: "Action" },
+  { text: "Comedy", value: "Comedy" },
+]);
+
+const validationRules = (rule) => {
+  if (rule === "required") return /^ *$/;
+
+  return null;
 };
 
+function validate() {
+  let valid = true;
+  clearErrors();
+  for (const [field, rule] of Object.entries(validations)) {
+    const validation = validationRules(rule);
+    if (validation) {
+      if (validation.test(form[field] || "")) {
+        errors[field] = `${field} is ${rule}`;
+        valid = false;
+      }
+    }
+  }
 
+  return valid;
+}
+
+
+
+const submitForm = () => {
+  if (validate()) {
+    const movie = {
+      id: Number(Date.now()),
+      name: form.name,
+      description: form.description,
+      image: form.image,
+      genres: form.genres,
+      inTheaters: form.inTheaters,
+      rating: null,
+    };
+    movies.push(movie);
+    hideForm();
+  }
+}
+
+function cleanUpForm() {
+  form.name = null;
+  form.description = null;
+  form.image = null;
+  form.genres = null;
+  form.inTheaters = false;
+  clearErrors();
+}
+
+function clearErrors() {
+  errors.name = null;
+  errors.description = null;
+  errors.image = null;
+  errors.genres = [];
+  errors.inTheaters = null;
+}
+
+const showMovieForm = ref(false);
+
+function hideForm() {
+  showMovieForm.value = false;
+  cleanUpForm();
+}
+
+function showForm() {
+  showMovieForm.value = true;
+}
 </script>
 
 <template>
-  <div class="flex flex-row justify-between w-full h-screen gap-4 p-16">
-    <div class="bg-slate-50" v-for="movie in movies" :key="movie.id">
-      <div class="bg-center bg-cover h-3/5" :style="{ backgroundImage: 'url(' + movie.image + ')' }">
-      </div>
-      <div class="flex flex-col justify-between gap-2 p-4">
-
-        <div class="text-2xl font-bold">{{ movie.name }}</div>
-        <div class="flex flex-wrap gap-2">
-          <span v-for="genre in movie.genres" :key="genre" class="px-2 py-1 text-white bg-blue-500 rounded">{{ genre
-          }}</span>
-        </div>
-        <div class="text-lg">{{ movie.description }}</div>
-        <div class="flex items-center gap-2 text-sm font-bold">Rating ({{ movie.rating }}/5)
-          <span class="flex">
-            <button @click="handleClick($event, 1)" class="flex disabled:cursor-not-allowed disabled:text-gray-400">
-              <StarIcon class="w-6 h-6 text-yellow-500 cursor-pointer" />
-            </button>
-            <button @click="handleClick($event, 2)" class="disabled:cursor-not-allowed disabled:text-gray-400">
-              <StarIcon class="w-6 h-6 text-yellow-500" />
-            </button>
-            <button @click="handleClick($event, 3)" class="disabled:cursor-not-allowed disabled:text-gray-400">
-              <StarIcon class="w-6 h-6 text-yellow-500" />
-            </button>
-            <button @click="handleClick($event, 4)" class="disabled:cursor-not-allowed disabled:text-gray-400">
-              <StarIcon class="w-6 h-6 text-yellow-500" />
-            </button>
-            <button @click="handleClick($event, 5)" class="disabled:cursor-not-allowed disabled:text-gray-400">
-              <StarIcon class="w-6 h-6 text-yellow-500" />
-            </button>
+  <div class="">
+    <!-- movie list-->
+    <div class="pt-40 movie-list">
+      <!--movie item-->
+      <div class="relative movie-item" v-for="(movie, movieIndex) in movies" :key="movie.id">
+        <span class="absolute z-10 grid w-12 h-2 top-4 right-4">
+          <StarIcon class="" :class="[
+            movie.rating > 0 ? 'text-yellow-500' : 'text-gray-500',
+          ]" />
+          <span class="absolute font-bold text-2xl top-1/2 right-1/2 translate-x-[50%] ">
+            {{ movie.rating ?? '-' }}
           </span>
+        </span>
+        <div class="movie-item-image-wrapper">
+          <img :src="movie.image" class="movie-item-image" alt="">
         </div>
+        <div class="flex flex-col justify-between gap-2 p-4">
+          <div class="text-2xl font-bold">{{ movie.name }}</div>
+          <div class="flex flex-wrap gap-2">
+            <span v-for="genre in movie.genres" :key="genre" class="px-2 py-1 text-white bg-blue-500 rounded">{{ genre
+            }}</span>
+          </div>
+          <div class="text-lg">{{ movie.description }}</div>
+          <div class="movie-item-star-icon-wrapper">
+            <span class="movie-item-rating-text">
+              Rating: ({{ movie.rating ?? 0 }}/5)
+            </span>
+            <button v-for="star in 5" :key="star" class="movie-item-star-icon-button" :class="[
+              star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
+            ]" :disabled="star === movie.rating" @click="updateRating(movieIndex, star)">
+              <StarIcon class="movie-item-star-icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- add movie button-->
+    <div class="fixed top-4 right-4">
+      <button class="p-4 text-2xl text-white bg-blue-500 rounded-full" @click="showForm">Add movie</button>
+      <div class=" form-modal" v-if="showMovieForm">
+        <form class="relative" @submit.prevent="submitForm">
+          <div class="cancel">
+            <button class="absolute px-2 text-2xl text-white bg-blue-500 rounded-full top-4 right-4"
+              @click="hideForm">X</button>
+          </div>
+          <div class="mt-10">
+            <div class="form-group">
+              <label for="name">Name:</label>
+              <input type="text" id="name" name="name" required v-model="form.name">
+            </div>
+            <div class="form-group">
+              <label for="description">Description:</label>
+              <textarea id="description" name="description" v-model="form.description"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="image">Image:</label>
+              <input type="text" id="image" name="image" v-model="form.image">
+            </div>
+            <div class="form-group">
+              <label for="genre">Genre:</label>
+              <select id="genre" name="genre" v-model="form.genres" multiple required>
+                <option value="action">Action</option>
+                <option value="comedy">Comedy</option>
+                <option value="drama">Drama</option>
+                <option value="horror">Horror</option>
+                <option value="sci-fi">Sci-Fi</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="theaters">Is in theathers:</label>
+              <input type="checkbox" id="featured" v-model="form.inTheaters" />
+            </div>
+            <div class="form-group">
+              <button type="submit" class="px-8 m-4 text-red-500 bg-white rounded-md">Submit</button>
+            </div>
+          </div>
+        </form>
+
       </div>
     </div>
   </div>
