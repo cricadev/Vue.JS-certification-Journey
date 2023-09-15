83d0e30084155ed548a547e712565ce1859d2235
CristianN998 <cristiancasallas998@gmail.com>
2023-09-15 13:11:36 -0500

    SEO with usehead

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "../router";
+import { createHead } from "@vueuse/head";
 import "../style.css";
 const app = createApp(App);
+app.use(createHead());
 app.use(router);
 app.mount("#app");
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,6 +14,21 @@
   </div>
 </template>
 <script setup>
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Vue.js Certification',
+  meta: [
+    {
+      name: 'description',
+      content: 'My journey towards becoming a certified Vue.js developer. Notes, projects and resources',
+    },
+    {
+      name: 'keywords',
+      content: 'Vue.js, Certification, Vue.js Certification',
+    },
+  ],
+})
 
 const challenges = [
   {
