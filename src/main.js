58a4e8a1a9edeb67b8d979ca4b94c198f4ec0321
CristianN998 <cristiancasallas998@gmail.com>
2023-09-15 13:16:09 -0500

    refactor head

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "../router";
 import { createHead } from "@vueuse/head";
 import "../style.css";
+const head = createHead();
+
 const app = createApp(App);
-app.use(createHead());
+app.use(head);
 app.use(router);
 app.mount("#app");
