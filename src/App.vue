51228eea93b94491793a7846d792dba9960b5cd2
CristianN998 <cristiancasallas998@gmail.com>
2023-09-15 13:14:34 -0500

    SEO app

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,22 @@
    <router-view></router-view>
  </div>
 </template>
-<script>
+<script setup>
+import { useHead } from '@vueuse/head'
 
+useHead({
+  title: 'Vue.js Certification',
+  meta: [
+    {
+      name: 'description',
+      content: 'My journey towards becoming a certified Vue.js developer. Notes, projects and resources',
+    },
+    {
+      name: 'keywords',
+      content: 'Vue.js, Certification, Vue.js Certification',
+    },
+  ],
+})
 </script>
 <style lang="">
   
